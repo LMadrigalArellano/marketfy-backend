7a3aed74a11f88de408e7c2b4a00c813d40d8219
LMadrigalArellano@Deloitte.com <LMadrigalArellano@Deloitte.com>
2024-04-02 12:28:31 -0600

    LDMA:
    Completed basic CRUD operations to UserController:
    
    GET:
    getAllUsers
    getUserById
    getUserByEmail
    
    POST:
    saveUser
    
    PATCH:
    updateUser
    
    DELETE:
    deleteUserById

--- marketfy/src/main/java/com/deloitte/marketfy/controllers/UserController.java
+++ marketfy/src/main/java/com/deloitte/marketfy/controllers/UserController.java
@@ -3,8 +3,14 @@ package com.deloitte.marketfy.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,14 +40,97 @@ public class UserController {
 	
 	@GetMapping("/users/{userId}")
 	public Optional<User> getUserById(@PathVariable("userId") int userId) {
-		
 		Optional<User> user = userRepository.findById(userId);		
-		
 		return user;
-		
+	}
+	
+	@GetMapping("/users/email/{email}")
+	public Optional<User> getUserByEmail(@PathVariable("email") String email) {		
+		Optional<User> user = userRepository.findByEmail(email);
+		return user;
 	}
 	
 	/////////////////////////////////---END 'GET' OPERATIONS---/////////////////////////////////
+	
+	/////////////////////////////////---START POST OPERATIONS---/////////////////////////////////
+
+	@PostMapping("/users")
+	public ResponseEntity<String> saveUser(@RequestBody User user) {
+	
+		ResponseEntity<String> result = new ResponseEntity<String>("EMAIL \""+user.getEmail()+"\" IS ALREADY REGISTERED", HttpStatus.CONFLICT);	;
+		boolean emailIsNotRegistered = getUserByEmail(user.getEmail()).isEmpty();
+		
+		if(emailIsNotRegistered) {
+			result = new ResponseEntity<String>("USER \""+user.getFirstName()+"\" CREATED", HttpStatus.CREATED);	
+			userRepository.save(user);
+		}
+		
+		
+		return result;
+	}
+
+	/////////////////////////////////---END POST OPERATIONS---/////////////////////////////////
+	
+	/////////////////////////////////---START PATCH OPERATIONS---/////////////////////////////////
+	
+	@PatchMapping("users/{userId}")
+	public ResponseEntity<String> updateUser(@PathVariable("userId") int userId, @RequestBody User newUser) {
+		
+		ResponseEntity<String> result = new ResponseEntity<>("USER WITH ID \""+userId+"\" UPDATED", HttpStatus.OK);
+		Optional<User> userInDB = getUserById(userId);
+		boolean isNewEmailAvailable = getUserByEmail(newUser.getEmail()).isEmpty();
+		
+		if(userInDB.isEmpty()) {
+			
+			return new ResponseEntity<String>("USER WITH ID \""+userId+"\" NOT FOUND", HttpStatus.NOT_FOUND);
+		
+		} else if(!isNewEmailAvailable) {
+			return new ResponseEntity<String>("EMAIL \""+newUser.getEmail()+"\" ALREADY REGISTERED", HttpStatus.CONFLICT);
+
+		} else {
+			userInDB.ifPresent(user -> {
+				if(newUser.getFirstName() != null) {
+					user.setFirstName(newUser.getFirstName());
+				}
+				if(newUser.getLastName() != null) {
+					user.setLastName(newUser.getLastName());
+				}
+				if(newUser.getBio() != null) {
+					user.setBio(newUser.getBio());
+				}
+				if(newUser.getEmail() != null) {
+					user.setEmail(newUser.getEmail());
+				}
+				if(newUser.getAreasOfInterest() != null) {
+					user.setAreasOfInterest(newUser.getAreasOfInterest());	
+				}
+			
+				userRepository.save(user);
+			});	
+		}
+	
+		return result;
+	}
+	
+	/////////////////////////////////---END PATCH OPERATIONS---/////////////////////////////////
+	
+	/////////////////////////////////---START DELETE OPERATIONS---/////////////////////////////////
+
+	@DeleteMapping("/users/delete/{userId}")
+	public ResponseEntity<String> deleteUserById(@PathVariable("userId") int userId){
+		
+		ResponseEntity<String> result = new ResponseEntity<String>("USER WITH ID \""+userId+"\" NOT FOUND", HttpStatus.NOT_FOUND);
+		
+		if(getUserById(userId).isPresent()) {
+			userRepository.deleteById(userId);
+			result = new ResponseEntity<String>("USER WITH ID \""+userId+"\" DELETED", HttpStatus.OK);	
+		}
+		
+		return result;
+		
+	}
+	
+	/////////////////////////////////---END DELETE OPERATIONS---/////////////////////////////////
 
 
 }
